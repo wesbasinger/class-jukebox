e1b339da7385ba09eb1058e7dcf85e8568868694
Wes Basinger <wbasinger@villagetechschools.org>
2018-06-01 20:21:51 -0500

    Created a skeleton and installed dependencies for mopidy.

--- src/index.js
+++ src/index.js
@@ -1,4 +1,59 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-ReactDOM.render(<h1>Hello world.</h1>, document.getElementById('root'));
+const Mopidy = require('mopidy');
+
+const mopidy = new Mopidy({
+  webSocketUrl: "ws://192.168.1.65:6680/mopidy/ws/",
+  callingConvention: "by-position-or-by-name"
+});
+
+console.log(mopidy)
+
+const Header = () => {
+  return(
+    <div>
+      <h1>Class Jukebox</h1>
+      <p>Use a valid code to play a song you would like to hear.</p>
+    </div>
+  )
+}
+
+const Footer = () => {
+  return(
+    <div>
+      <p>Created and maintained by Wes Basinger.  Contact wbasinger@villagetechschools.org with any issues.</p>
+      <p>Powered by Mopidy.</p>
+    </div>
+  )
+}
+
+
+
+class App extends React.Component {
+
+  constructor(props) {
+    super(props)
+
+    this.state = {
+      tracks: []
+    }
+  }
+
+  componentDidMount() {
+
+  }
+
+  render() {
+    return(
+      <div>
+        <Header />
+          <div>Main content goes here.</div>
+        <Footer />
+      </div>
+    )
+  }
+
+}
+
+ReactDOM.render(<App />, document.getElementById('root'));
